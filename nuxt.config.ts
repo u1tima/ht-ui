b03381b5f95ef79d8b38ab8fd71b2422e3dffecd
Vitali <vit-sema@yandex.ru>
2026-05-12 09:23:25 +0300

    fix site redirect for local and prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,10 @@ export default defineNuxtConfig({
 
 	runtimeConfig: {
 		public: {
-			siteUrl: process.env.NUXT_PUBLIC_SITE_URL ?? '',
+			// Set to production URL on Vercel, localhost locally
+			siteUrl: process.env.NODE_ENV === 'production'
+				? 'https://ht-ui.vercel.app/'
+				: 'http://localhost:3000'
 		},
 	},
 
